563cecf1a4998565d15348dfc9d2b19129ba890e
Alessandro <me@alexstoneham.co.uk>
2025-04-19 02:13:11 +0100

    hopefully fixed

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./", // Using relative path for GitHub Pages
+  base: "/", // Using relative path for GitHub Pages
   server: {
     host: "::",
     port: 8080,
